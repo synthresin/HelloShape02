02fa0edd89e238c7196f215294ba1c56a65f1ac0
synthresin <synthresin@me.com>
2012-09-09 00:15:45 +0900

    각도조절과 좀더 마우스 위치에 따른 크기 조절

--- src/HelloShape02App.cpp
+++ src/HelloShape02App.cpp
@@ -1,7 +1,7 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 
-#define MODE_NUM 10
+#define MODE_NUM 5
 
 using namespace ci;
 using namespace ci::app;
@@ -14,6 +14,7 @@ class HelloShape02App : public AppBasic {
     void mouseUp  ( MouseEvent event );
 	void mouseDown( MouseEvent event );
 	void mouseDrag( MouseEvent event );
+    void keyDown  ( KeyEvent event );
 	void update();
 	void draw();
     
@@ -22,14 +23,22 @@ class HelloShape02App : public AppBasic {
     float mModeDomainY;
     
     bool mDraw;
+    bool mRight;
+    
+    float mOpacity;
 };
 
 void HelloShape02App::setup()
 {
     mModeDomainY = (float)getWindowHeight() / MODE_NUM;
+    mOpacity = 0.1;
     
     gl::enableAlphaBlending();
     gl::clear( Color( 255, 255, 255 ) );
+    
+    mRight = false;
+    gl::color(1, 0.69f, 0.2f, mOpacity);
+    glLineWidth(2.5f);
 }
 
 void HelloShape02App::mouseMove( MouseEvent event )
@@ -44,6 +53,11 @@ void HelloShape02App::mouseMove( MouseEvent event )
             mSegment = i + 3;
         }
     }
+    
+    int xpos = event.getX();
+    if(xpos > getWindowWidth()/2) {
+        mRight = true;
+    } else mRight = false;
     //glLineWidth(1.0f);
 }
 
@@ -65,18 +79,41 @@ void HelloShape02App::mouseDrag( MouseEvent event )
     //mouseDown(event);
 }
 
+void HelloShape02App::keyDown( KeyEvent event )
+{
+    if(event.getChar() == '1') {
+        gl::color(1, 0.69f, 0.2f, mOpacity);
+    } else if(event.getChar() == '2') {
+        gl::color(0.28, 0.71, 0.14, mOpacity);
+    } else if(event.getChar() == '3') {
+        gl::color(0.09f, 0.48, 0.48, mOpacity);
+    } else if(event.getChar() == '4') {
+        gl::color(0, 0, 0, mOpacity);
+    } else if(event.getChar() == '5') {
+        gl::clear(Color(1,1,1));
+    }
+}
+
 void HelloShape02App::update()
 {
-    printf("mSegment: %i \n", mSize/200);
+ //   printf("mSegment: %i \n", mSize/200);
 }
 
 void HelloShape02App::draw()
 {
 	// clear out the window with black
 	// gl::clear( Color( 0, 0, 0 ) );
-    gl::color(0, 0, 0, 0.05);
-    if(mDraw)
-    gl::drawStrokedCircle(getWindowCenter(), mSize/200, mSegment);
+    if(mDraw) {
+        gl::pushMatrices();
+        
+        gl::translate(getWindowCenter());
+        if(mRight) gl::rotate(45);
+        else gl::rotate(315);
+        
+        gl::drawStrokedCircle(Vec2f::zero(), sqrt(mSize), mSegment);
+        gl::popMatrices();
+    }
+    
     
     
 }
